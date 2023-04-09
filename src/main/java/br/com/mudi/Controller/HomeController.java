28a7f2ae467bb89970a860585f40c4f8a68e3f78
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2023-04-09 13:14:36 -0300

    [Home] [REFACTOR] Performance improvement caused by cache

--- src/main/java/br/com/mudi/Controller/HomeController.java
+++ src/main/java/br/com/mudi/Controller/HomeController.java
@@ -4,6 +4,8 @@ import br.com.mudi.Model.Order;
 import br.com.mudi.Model.Status;
 import br.com.mudi.Repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,11 @@ public class HomeController {
     @GetMapping
     public ModelAndView home(Principal principal) {
 
-        List<Order> orders = orderRepository.findByStatus(Status.ENTREGUE);
+        Sort sort = Sort.by("deliveryDate").descending();
+
+        PageRequest pageable = PageRequest.of(0, 10, sort);
+
+        List<Order> orders = orderRepository.findByStatus(Status.ENTREGUE, pageable);
 
         ModelAndView mv = new ModelAndView("home");
 
--- src/main/java/br/com/mudi/Repository/OrderRepository.java
+++ src/main/java/br/com/mudi/Repository/OrderRepository.java
@@ -2,6 +2,8 @@ package br.com.mudi.Repository;
 
 import br.com.mudi.Model.Order;
 import br.com.mudi.Model.Status;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +13,8 @@ import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findByStatus(Status status);
+    @Cacheable("orders")
+    List<Order> findByStatus(Status status, Pageable pageable);
 
     @Query("select o from Order o join o.user u where u.username = :username")
     List<Order> findAllByUser(@Param("username") String username);
