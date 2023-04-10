85d8605f603d813248cf7c441dbd9c75e131e8cb
CarlosAMF02 <c.a.mascarenhas@hotmail.com>
2023-04-10 16:24:29 -0300

    [Offer] [CHORE] List of database waiting offers in Offers home page

--- src/main/java/br/com/mudi/Api/OrderRest.java
+++ src/main/java/br/com/mudi/Api/OrderRest.java
@@ -1,14 +1,17 @@
 package br.com.mudi.Api;
 
 import br.com.mudi.Model.Order;
+import br.com.mudi.Model.Status;
 import br.com.mudi.Repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -25,6 +28,22 @@ public class OrderRest {
         PageRequest pageable = PageRequest.of(0, 10, sort);
 
         List<Order> orders = orderRepository.findAll();
+        return orders;
+    }
+
+    @GetMapping("{status}")
+    public List<Order> getWaitingOrders(@PathVariable String status) {
+
+        Sort sort = Sort.by("deliveryDate").descending();
+        PageRequest pageable = PageRequest.of(0, 10, sort);
+        List<Order> orders = new ArrayList<Order>();
+        try {
+            orders = orderRepository.findByStatus(Status.valueOf(status.toUpperCase()), pageable);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return orders;
     }
 
